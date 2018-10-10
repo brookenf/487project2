cc8e7736bfcb49bb7ca7095d4d4e0b813881a85d
Brooke Fisher <brookenfish1@gmail.com>
2018-10-10 13:13:30 -0400

    Created different grids to add in the overall flow of the project

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,14 @@
 $(function(){
   console.log('DOM is loaded');
 
-  //initializing typed.js
-  var typed = new Typed('#typed',{
-    stringsElement: '#typed-strings',
-    typeSpeed: 30,
-    backSpeed: 5,
-    loop: false
-  });
-
-
   $('#pagepiling').pagepiling({
-	    menu: null,
+	       menu: null,
         direction: 'vertical',
         verticalCentered: true,
         sectionsColor: ['#ffb6c1', '#FF6980', '#7F3540', '#CC929A', '#7F5B60', '#FF6980', '#ffb6c1'],
-        anchors: ['MeToo', 'History', 'BelieveSurvivors', 'Kavanaugh', 'TimesUp', 'Quiz', 'Sources'],
+        anchors: ['MeToo', 'History', 'BelieveWomen', 'Kavanaugh', 'TimesUp', 'Quiz', 'Sources'],
         scrollingSpeed: 700,
-        easing: 'swing',
+        easing: 'easeInQuart',
         loopBottom: true,
         loopTop: false,
         css3: true,
@@ -45,6 +36,14 @@ $(function(){
 		onLeave: function(index, nextIndex, direction){},
 		afterLoad: function(anchorLink, index){},
 		afterRender: function(){
+      //initializing typed.js
+      var typed = new Typed('#typed',{
+        stringsElement: '#typed-strings',
+        typeSpeed: 30,
+        backSpeed: 5,
+        loop: false
+      });
+
       //initializing the quiz
       $('#slickQuiz').slickQuiz({
         // options
--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -5,22 +5,22 @@
 var quizJSON = {
     "info": {
         "name":    "Test Your Knowledge!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "main":    "<p>Feminist knowledge in five easy steps</p>",
+        "results": "<h5>Learn More</h5><p>While a lot of good has happened in the past year because of #MeToo, we still have a long way to go until we reach full equality between men and women. Keep up the good fight.</p>",
+        "level1":  "Feminist SLAYER",
+        "level2":  "You\'ve got the equality sticker on your car, right?",
+        "level3":  "You must read theSkimm",
+        "level4":  "Progress is one step forward...",
+        "level5":  "You might need to go back and re-read this entire web app" // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "When did the term 'Me Too' first get used?",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "2013",      "correct": false},
+                {"option": "2017",     "correct": false},
+                {"option": "2006",      "correct": true},
+                {"option": "2003",     "correct": false} // no comma here
             ],
             "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
             "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
