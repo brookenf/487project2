5a864597b58600bc75c0ec4262fcadd8c89212c3
Brooke Fisher <brookenfish1@gmail.com>
2018-10-10 06:39:43 -0400

    Added in a cool typing feature on the homepage to offer dynamic first screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
+//Copyright (c) 2014 Alvaro Trigo <alvaro@alvarotrigo.com>
+
 $(function(){
   console.log('DOM is loaded');
+
+  //initializing typed.js
+  var typed = new Typed('#typed',{
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    backSpeed: 5,
+    loop: false
+  });
+
 
   $('#pagepiling').pagepiling({
 	    menu: null,
         direction: 'vertical',
         verticalCentered: true,
-        sectionsColor: ['#ffb6c1', '#ffb6c1', '#ffb6c1', '#ffb6c1', '#ffb6c1', '#ffb6c1', '#ffb6c1'],
-        anchors: [],
+        sectionsColor: ['#ffb6c1', '#FF6980', '#7F3540', '#CC929A', '#7F5B60', '#FF6980', '#ffb6c1'],
+        anchors: ['MeToo', 'History', 'BelieveSurvivors', 'Kavanaugh', 'TimesUp', 'Quiz', 'Sources'],
         scrollingSpeed: 700,
-        easing: 'linear',
+        easing: 'swing',
         loopBottom: true,
-        loopTop: true,
+        loopTop: false,
         css3: true,
         navigation: {
             'textColor': '#a1a1a1',
@@ -33,13 +44,15 @@ $(function(){
 		//events
 		onLeave: function(index, nextIndex, direction){},
 		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
+		afterRender: function(){
+      //initializing the quiz
+      $('#slickQuiz').slickQuiz({
+        // options
+      });
+    },
 	});
 
-  //initializing the quiz
-  $('#slickQuiz').slickQuiz({
-        // options
-    });
+
 
 
 //*closing of the document.ready
